957391d19db760f865d1773287de90e4fb3d6671
Stephan Müller <mail@stephanmueller.eu>
2017-04-02 18:36:32 +0200

    git tagging after uploading to pypi

--- publish.sh
+++ publish.sh
@@ -2,12 +2,6 @@
 
 VERSION=$(python3 -c "import modbusreader; print(modbusreader.__version__)")
 
-echo "creating tag $VERSION"
-git tag $VERSION
-
-echo "pushing tag"
-git push ${REMOTE:-origin} $VERSION
-
 if [ -f MANIFEST ]; then
     echo "removing manifest file"
     rm MANIFEST
@@ -16,3 +10,9 @@ fi
 echo "uploading package."
 python3 setup.py sdist
 twine upload dist/*
+
+echo "creating tag $VERSION"
+git tag $VERSION
+
+echo "pushing tag"
+git push ${REMOTE:-origin} $VERSION
