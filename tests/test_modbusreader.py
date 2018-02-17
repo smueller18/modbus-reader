5a2ec0c9998ed9812977def145fd4b6de5d213d8
Stephan Müller <mail@stephanmueller.eu>
2018-02-17 17:43:40 +0100

    add test for ModbusReader

--- tests/test_modbusreader.py
+++ tests/test_modbusreader.py
@@ -1,3 +1,25 @@
+import unittest2
+import json
+import os
+
 from modbusreader import ModbusReader
 
-#modbus = ModbusReader("localhost", 502, 1, "config.json")
+__author__ = "Stephan Müller"
+__copyright__ = "2017, Stephan Müller"
+__license__ = "MIT"
+
+__dirname__ = os.path.dirname(os.path.abspath(__file__))
+
+class ModbusReaderTests(unittest2.TestCase):
+
+    CONFIG = "config.json"
+
+    @classmethod
+    def setUpClass(cls):
+        #cls.modbusreader = ModbusReader("localhost", 502, 0, "config.json")
+        pass
+
+    def test_goup_modbus_device_definition(self):
+        ModbusReader.group_modbus_device_definition(
+            json.loads(open(__dirname__ + "/" + self.CONFIG, 'rb').read().decode("UTF-8"))
+        )
